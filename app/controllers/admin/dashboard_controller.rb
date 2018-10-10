407d8b407fb9482d3b3294c1fe7713f6f0b0559f
florian.marchal <florian.marchal@coupa.com>
2018-10-10 11:55:23 +0200

    first commit

---
+++ app/controllers/admin/dashboard_controller.rb
@@ -0,0 +1,12 @@
+module Admin
+  class DashboardController < ApplicationController
+    def main
+    end
+
+    def user
+    end
+
+    def blog
+    end
+  end
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,11 @@
+class PostsController < ApplicationController
+  def index 
+  end
+
+  def new
+  end
+
+  def missing
+    
+  end
+end
